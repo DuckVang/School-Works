1d859f6c5f2cde1ec4e0b1e06ec1c4fbbf21c87e
Nygen <nducminh002@gmail.com>
2023-02-13 11:33:50 +0100

    Logging-Add: inviduals buttons basic structure

---
+++ Logging/src/components/Info.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    methods: {
+
+        handle() {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="handle"></button>
+</template>
+
--- Logging/src/stores/counter.js
+++ Logging/src/stores/counter.js
@@ -1,7 +1,10 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore("counter", () => {
+
+
+
+export const useLogsStore = defineStore("logs", () => {
   const count = ref(0);
   const doubleCount = computed(() => count.value * 2);
   function increment() {
