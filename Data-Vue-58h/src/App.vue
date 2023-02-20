ddd85b9174d70f359e2874e496f05237bc7eeccb
Nygen <nducminh002@gmail.com>
2023-02-20 18:11:08 +0100

    init du-logging

---
+++ Data-Vue-58h/src/App.vue
@@ -0,0 +1,75 @@
+<script >
+import Logger from './components/Logger.vue';
+
+export default {
+
+  components: {
+    Logger
+  },
+
+  data() {
+
+    return {
+      messages: []
+
+
+
+    }
+
+  },
+  methods: {
+    addMessage(message) {
+      this.messages.push(message)
+    }
+  }
+
+}
+</script>
+
+<template>
+  <header>
+    <Logger name="Varování" @addMessage="addMessage" />
+    <Logger name="Informace" @addMessage="addMessage" />
+    <Logger name="Error" @addMessage="addMessage" />
+  </header>
+  <main>
+
+    <p v-for="message in messages" :key="messages.length">{{ message.name }}: {{ message.message }}</p>
+
+  </main>
+</template>
+
+<style scoped>
+<
+header{
+
+  display: flex;
+  justify-content: space-between;
+
+}
+main {
+  margin-top: 10px;
+  text-align: left;
+  border: black solid 1px;
+  height: 75vh;
+  width: 25vw;
+  overflow: scroll;
+  padding: 20px;
+;
+}
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ Data-Vue-58h/src/components/Logger.vue
@@ -0,0 +1,32 @@
+<script>
+
+
+export default {
+  props: {
+    name: String
+
+  },
+  data() {
+
+    return {
+
+
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$emit("addMessage", { name: this.name, message: "This is message from " + this.name })
+    }
+
+  }
+
+}
+
+
+</script>
+
+<template>
+  <button @click="sendMessage">{{ name }}</button>
+</template>
+
+<style scoped></style>
