4339ad3582baa85688188e72512b86bd219df882
Nygen <nducminh002@gmail.com>
2023-02-26 13:47:39 +0100

    Finished logger hw

---
+++ Logger/src/App.vue
@@ -0,0 +1,79 @@
+<script >
+import Logger from './components/Logger.vue';
+
+export default {
+
+  components: {
+    Logger
+  },
+
+  data() {
+
+    return {
+      messages: []
+
+
+
+    }
+
+  },
+  methods: {
+    addMessage(message) {
+      this.messages.push(message)
+    }
+  }
+
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <header>
+      <Logger name="Varování" type="warn" @addMessage="addMessage" />
+      <Logger name="Informace" type="info" @addMessage="addMessage" />
+      <Logger name="Error" type="error" @addMessage="addMessage" />
+    </header>
+    <main>
+
+      <p v-for="message in messages" :key="messages.length"><b> {{ message.type }}</b>: {{ message.message }}</p>
+
+    </main>
+  </div>
+</template>
+
+<style scoped >
+header {
+
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+
+main {
+  margin-top: 10px;
+  text-align: left;
+  border: black solid 1px;
+  height: 100%;
+
+  overflow: scroll;
+  padding: 20px;
+  width: 100%;
+  background-color: rgb(207, 228, 235);
+  border: 2px solid cadetblue;
+}
+main>p {
+  margin: 2px;
+  padding: 0;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  width: 40vw;
+
+}
+</style>
---
+++ Logger/src/components/Logger.vue
@@ -0,0 +1,59 @@
+<script>
+
+
+export default {
+  props: {
+    name: String,
+    type: String
+
+  },
+  data() {
+
+    return {
+
+
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$emit("addMessage", { type: this.type, message: "Toto je testovací zpráva" })
+    }
+
+  }
+
+}
+
+
+</script>
+
+<template>
+  <button @click="sendMessage">
+    <h2>{{ name }}</h2>
+  </button>
+</template>
+
+<style scoped>
+button {
+
+
+  width: 120px;
+  height: fit-content;
+
+  border: 2px solid orange;
+
+  background-color: rgb(219, 174, 3);
+
+  padding: 20px;
+
+  display: flex;
+  text-align: center;
+  justify-content: center;
+}
+button:active{
+  background-color: rgba(210, 176, 3, 0.511);
+}
+h2{
+  margin: 0;
+  padding: 0;
+}
+</style>
